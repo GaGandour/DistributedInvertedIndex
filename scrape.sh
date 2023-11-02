8f9d0b72c31797ba2a844989d6f4e5fb62c4c6ee
Gabriel Gandour <gagandour@gmail.com>
2023-11-02 17:42:08 -0300

    update scrape

--- scrape.sh
+++ scrape.sh
@@ -1,1 +1,2 @@
-for counter in $(seq 1 4); do curl https://www.gutenberg.org/cache/epub/$counter/pg$counter.txt > books/pg$counter.txt; done
+mkdir -p books
+for counter in $(seq 1 $1); do curl https://www.gutenberg.org/cache/epub/$counter/pg$counter.txt > books/pg$counter.txt; done
